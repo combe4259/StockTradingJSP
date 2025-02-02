940314a41393a4f50e7539e1604d3f918b661f0f
han97901 <han97901@gmail.com>
2025-02-02 21:59:53 +0900

    로그인 session없이 구현

---
+++ src/main/java/org/zerock/stocktrading/controller/MainController.java
@@ -0,0 +1,25 @@
+package org.zerock.stocktrading.controller;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/main")
+@Log4j2
+public class MainController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        log.info("main get....");
+
+
+        req.getRequestDispatcher("/WEB-INF/main.jsp").forward(req, resp);
+
+
+    }
+}
